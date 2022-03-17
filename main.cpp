2409f69f0a997c5e1683d5535285198361ae9669
NikolaArden <anthonyardenuk@gmail.com>
2022-03-17 14:18:36 +0000

    update

--- main.cpp
+++ main.cpp
@@ -1,16 +1,48 @@
 #include <iostream> // cout; cin
 #include <string>   // strings
 
+// COMMENTING SHORTCUT
+// select the lines you wish to comment/uncomment
+// "CTRL" + "/"
+
 int main() {
     // ===============================================================================================
     // {5} example 3 - dereferencing a pointer
-    int year = 2022;			// (1)
-    int* p_year = &year; 		// (2) reference year
-    int age = *p_year - 1998;	// (3) dereference p_year
-    std::cout << "Variable value:       " << year << std::endl;
-    std::cout << "Referenced variable (address): " << &year << std::endl;
-    std::cout << "Pointer value:        " << p_year << std::endl;
-    std::cout << "Dereferenced Pointer: " << p_year << std::endl;
+    // int year = 2022;			// (1)
+    // int* p_year = &year; 		// (2) reference year
+    // int age = *p_year - 1998;	// (3) dereference p_year
+    // std::cout << "Variable value:       " << year << std::endl;
+    // std::cout << "Referenced variable:  " << &year << std::endl;
+    // std::cout << "Pointer value:        " << p_year << std::endl;
+    // std::cout << "Dereferenced Pointer: " << *p_year << std::endl;
+
+    // ===============================================================================================
+    // {6} exercise 3 - dereferencing a pointer
+    // DO THIS EXERCISE !
+
+    // ===============================================================================================
+    // {7} homework 1 - dereferencing a pointer
+    // DO THIS AT HOME !
+        // In this homework we will swap two pointers. 
+        // 1)	Create two pointers to two different variables. 
+        // 2)	Now swap these pointers (without using the variables).
+        // [Hint: You will need to create a temporary pointer]
+    
+    // ===============================================================================================
+    // {8} example 4 - constants and pointers
+    // std::string msg {"Hello Naliko!"};     // string
+    // const std::string cns_msg {"This message cannot be changed!"}; // constant string
+
+    // std::string* ptr1 {&msg};               // (string) pointer
+    // const std::string* ptr2 {&cns_msg};     // (constant string) pointer
+
+    // std::string* const ptr3 {&msg};         // (string) constant pointer
+    // const std::string* const {&cns_msg};    // (constant string) constant pointer
+
+    // ===============================================================================================
+    // {9} exercise 5 - pointers
+    // DO THIS NOW !
+    
 
 
 };
